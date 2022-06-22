84c939d688e4701e0d9c0356a19aa05e4f1e5eac
myleerm <myleerm@gmail.com>
2022-06-22 13:20:50 -0500

    second commit

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,34 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hello and welcome!";
+let name = 'Mylee';
+let title = 'Captain of my own Ghost ship';
+let worldName = 'Marauder Valley';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 153;
+let year = 3000;
+let century = 30;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let hobbies = ['looting', 'counting rewards', 'having parties', 'fighting off enemies'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavActivity = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " My name is" +  name);
+console.log("I'm the" +  title + ".");
+console.log("I'm also one of the many pirates in" +  worldName + ".");
+console.log("In" +  worldName + ", time works a little differently.");
+console.log("You see, I'm" +  age + " years old, and technically dead, you know, a ghost ship?")
+console.log("We're also in the year" +  year + ", which makes it the" +  century + "th century.")
+console.log("I also have my fair share of hobbies.")
+console.log("I like to go" +  hobbies + "." )
+console.log("Listener: Do you have a favorite hobby?")
+console.log("Me:" +  hasFavActivity + ", I like " + hobbies[3] + " the most.")
+console.log("If you don't have any more questions, you can have fun exploring the ship!")
